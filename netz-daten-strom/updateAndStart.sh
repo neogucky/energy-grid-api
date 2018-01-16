36771ef233cf7bf28b386853be1db551147d5870
Tim Rasim <neogucky@gmail.com>
2018-01-16 14:01:22 +0100

    fixed start script parameters

--- netz-daten-strom/updateAndStart.sh
+++ netz-daten-strom/updateAndStart.sh
@@ -4,4 +4,4 @@ git checkout origin/master
 mongod &
 mongorestore -d deployd mongo_db_dump/deployd/
 sleep 1
-dpd -dbname deployd -host localhost --mongoPort 27017
+dpd --dbname deployd --host localhost --mongoPort 27017
