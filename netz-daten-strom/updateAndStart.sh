453c05f893d637553f8a0fc3a279053d9c4e440d
Tim Rasim <neogucky@gmail.com>
2018-01-16 16:59:10 +0100

    added sleep to start script to make sure mongod is running

--- netz-daten-strom/updateAndStart.sh
+++ netz-daten-strom/updateAndStart.sh
@@ -2,6 +2,7 @@
 git pull
 git checkout origin/master
 mongod &
-mongorestore -d deployd mongo_db_dump/deployd/ --host localhost --port 27017
+sleep 3
+mongorestore -d deployd mongo_db_dump/deployd/
 sleep 3
 dpd --dbname deployd --host localhost --mongoPort 27017
